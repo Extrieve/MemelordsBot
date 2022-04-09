55548a22ef4d1831a94075e7ef9922d50f318ee4
Nick <nicolas.cinera@gmail.com>
2022-04-08 23:38:33 -0400

    Update Anime.py
    - WIP

--- cogs/Anime.py
+++ cogs/Anime.py
@@ -16,5 +16,18 @@ class Anime(commands.Cog):
         """
         await ctx.send(f'{anime_name}')
 
+    @commands.command(name='search-anime', aliases=['search-ani'])
+    async def search_anime(self, ctx, *, anime_name):
+        """
+        Search for an anime.
+        """
+        if anime_name == 'help':
+            await ctx.send('You can search for an anime by typing `!search-anime <anime name>`')
+        if len(anime_name) <= 3:
+            await ctx.send('Please enter a longer anime name.')
+            
+        url = 'https://api.jikan.moe/v4/anime'
+
+
 def setup(bot):
     bot.add_cog(Anime(bot))
