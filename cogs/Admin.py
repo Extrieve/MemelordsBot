1e5c76921607f9b467ff9fda3b546bdbb10381f4
Nick <nicolas.cinera@gmail.com>
2022-04-09 22:07:28 -0400

    Continuing the Refactoring Process
    - Admin process started
    - Search process started

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -4,10 +4,65 @@ from discord.ext import commands
 import discord
 
 class Admin(commands.Cog):
-    """The description for Admin goes here."""
+    """Administrative command list"""
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name='kick', aliases=['k'], pass_context=True)
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        """Kick a member from the server."""
+        await member.kick(reason=reason)
+        await ctx.send(f'Kicked {member.mention}')
+
+    @commands.command(name='ban', aliases=['b'], pass_context=True)
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        """Ban a member from the server."""
+        await member.ban(reason=reason)
+        await ctx.send(f'Banned {member.mention}')
+
+    @commands.command(name='unban', aliases=['ub'], pass_context=True)
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        """Unban a member from the server."""
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                return
+            
+        await ctx.send(f'Could not find {member} in the ban list.')
+
+    @commands.command(name='clear', aliases=['c'], pass_context=True)
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount=5):
+        """Clear a certain amount of messages."""
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f'Cleared {amount} messages.')
+
+    @commands.command(name='mute', aliases=['m'], pass_context=True)
+    @commands.has_permissions(manage_roles=True)
+    async def mute(self, ctx, member: discord.Member, *, reason=None):
+        """Mute a member from the server."""
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        await member.add_roles(role, reason=reason)
+        await ctx.send(f'Muted {member.mention}')
+
+    @commands.command(name='unmute', aliases=['um'], pass_context=True)
+    @commands.has_permissions(manage_roles=True)
+    async def unmute(self, ctx, member: discord.Member, *, reason=None):
+        """Unmute a member from the server."""
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        await member.remove_roles(role, reason=reason)
+        await ctx.send(f'Unmuted {member.mention}')
+    
 
 def setup(bot):
     bot.add_cog(Admin(bot))
--- cogs/Search.py
+++ cogs/Search.py
@@ -2,12 +2,51 @@
 
 from discord.ext import commands
 import discord
+import sys
+import os
+from serpapi import GoogleSearch
 
 class Search(commands.Cog):
     """The description for Search goes here."""
 
+    # Get current working directory and add it to the path
+    cwd = os.getcwd()
+    sys.path.append(f'{cwd}..')
+    from config import serp_key
+
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name='image', aliases=['img'])
+    async def image_search(self, ctx, *, search):
+        """
+        Search for an image on Google Images.
+        """
+        
+        if not search:
+            return await ctx.send('Please enter a search term.')
+
+        params = {
+            'q': search,
+            'tbm': 'isch',
+            'ijn': '0',
+            'api_key': self.serp_key
+        }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        
+        # Tell user we're loading the request
+        await ctx.send(f'Loading...')
+
+        if not results:
+            return await ctx.send(f'No results found for {search}.')
+    
+        # await ctx.send(results['images_results'][0]['original'])
+
+        # Display the first image but allow the user to traverse the rest
+        embed = discord.Embed(title=f'{results["images_results"][0]["title"]}', url=results['images_results'][0]['original'], color=0x00ff00)
+        embed.set_image(url=results['images_results'][0]['original'])
+        return await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Search(bot))
