8699e7a0008443b7d9e3bc27bbc5a8a9182e8f25
Nick <nicolas.cinera@gmail.com>
2022-04-23 20:57:39 -0400

    OPGG Automation
    - TODO: Complete the latter half of the functionality

--- cogs/Automation.py
+++ cogs/Automation.py
@@ -7,12 +7,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import discord
+import asyncio
 
 class Automation(commands.Cog):
     """Cog for automation purposes with Selenium and Beautiful Soup."""
 
+    opt = webdriver.ChromeOptions()
+    opt.add_argument('--headless')
+    opt.add_argument('--disable-gpu')
+    opt.add_argument('--no-sandbox')
+    opt.add_argument('--disable-dev-shm-usage')
+    resolution = "--window-size=1920,1080"
+    opt.add_argument(resolution)
     service = Service(r'C:\Selenium\chromedriver_win32\chromedriver.exe')
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=opt)
+
+    # op gg regions
+    regions = {'na': ('North America', '🇺🇸'), 'euw': ('Europe West', '🇪🇺'), 'br': (
+        'Brazil', '🇧🇷'), 'las': ('LAS', '🇵🇪'), 'lan': ('LAN', '🇲🇽')}
+
+    flags = [item[1] for item in regions.values()]
 
     def __init__(self, bot):
         self.bot = bot
@@ -52,6 +66,39 @@ class Automation(commands.Cog):
 
         return await ctx.send(links[0])
 
+    @commands.command(name='opgg')
+    async def opgg(self, ctx, *, summoner):
+        """Get your OP.GG stats"""
+        if not summoner:
+            return await ctx.send('Please provide a summoner name')
+        
+        # send embed with the regions and their respective flags next to it
+        embed = discord.Embed(title='OP.GG', description='Select a region', color=0x00ff00)
+        for region, values in self.regions.items():
+            embed.add_field(name=region, value=values[1], inline=True)
+        msg = await ctx.send(embed=embed)
+
+        # add reactions to the message
+        for region in self.regions:
+            await msg.add_reaction(self.regions[region][1])
+
+        # wait for a reaction
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) in self.flags
+        
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            return await ctx.send('You took too long to react, cancelling...')
+
+        # get the region from the reaction
+        region = [key for key, value in self.regions.items() if value[1] == str(reaction.emoji)][0]
+
+        url = f'https://{region}.op.gg'
+        await ctx.send(url)
+        
+
+    
 
 def setup(bot):
     bot.add_cog(Automation(bot))
